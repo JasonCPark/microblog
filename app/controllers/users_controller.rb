000f659873e31de5ef2fbbf03bbe6572d5d5b9ae
Jason Park <jp.wakening@gmail.com>
2015-05-18 22:26:11 +0000

    users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   
+  def index
+    @users = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -52,7 +56,7 @@ class UsersController < ApplicationController
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      @microposts = @user.microposts.paginate(page: params[:page])
     end
     
 end
